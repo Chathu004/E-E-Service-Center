92595e523889803ba64b8bb0dc24702b1e7c518d
Chathuri Imesha <imeshachathuri6@gmail.com>
2024-01-21 10:37:21 +0530

    completely functional in customerForm

--- src/main/java/controller/CustomersFormController.java
+++ src/main/java/controller/CustomersFormController.java
@@ -1,16 +1,27 @@
 package controller;
 
+
+import bo.BoFactory;
+import bo.custom.CustomerBo;
+import dao.util.BoType;
+import dto.CustomerDto;
+import dto.tm.CustomerTm;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.List;
+
 
 public class CustomersFormController {
     @FXML
@@ -29,22 +40,86 @@ public class CustomersFormController {
     private TextField txtEmail;
 
     @FXML
-    private TableView<?> tblCustomer;
+    private TableView<CustomerTm> tblCustomer;
 
     @FXML
-    private TableColumn<?, ?> colId;
+    private TableColumn colId;
 
     @FXML
-    private TableColumn<?, ?> colName;
+    private TableColumn colName;
 
     @FXML
-    private TableColumn<?, ?> colContact;
+    private TableColumn colContact;
 
     @FXML
-    private TableColumn<?, ?> colEmail;
+    private TableColumn colEmail;
 
     @FXML
-    private TableColumn<?, ?> colOption;
+    private TableColumn colOption;
+    private CustomerBo customerBo = BoFactory.getInstance().getBo(BoType.CUSTOMER);
+
+    public void initialize(){
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colContact.setCellValueFactory(new PropertyValueFactory<>("contactNumber"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colOption.setCellValueFactory(new PropertyValueFactory<>("btn"));
+        loadCustomerTable();
+
+        tblCustomer.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
+            setData(newValue);
+        });
+    }
+    private void setData(CustomerTm newValue) {
+        if (newValue != null) {
+            txtID.setEditable(false);
+            txtID.setText(newValue.getId());
+            txtName.setText(newValue.getName());
+            txtContact.setText(newValue.getContactNumber());
+            txtEmail.setText(newValue.getEmail());
+        }
+    }
+    private void loadCustomerTable() {
+        ObservableList<CustomerTm> tmList = FXCollections.observableArrayList();
+
+        try {
+            List<CustomerDto> dtoList  = customerBo.allCustomers();
+            for (CustomerDto dto:dtoList) {
+                Button btn = new Button("Delete");
+                CustomerTm c = new CustomerTm(
+                        dto.getId(),
+                        dto.getName(),
+                        dto.getContactNumber(),
+                        dto.getEmail(),
+                        btn
+                );
+
+                btn.setOnAction(actionEvent -> {
+                    deleteCustomer(c.getId());
+                });
+
+                tmList.add(c);
+            }
+            tblCustomer.setItems(tmList);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void deleteCustomer(String id) {
+
+        try {
+            boolean isDeleted = customerBo.deleteCustomer(id);
+            if (isDeleted){
+                new Alert(Alert.AlertType.INFORMATION,"Customer Deleted!").show();
+                loadCustomerTable();
+            }else{
+                new Alert(Alert.AlertType.ERROR,"Something went wrong!").show();
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public void backBtnOnAction(javafx.event.ActionEvent actionEvent) {
@@ -58,17 +133,58 @@ public class CustomersFormController {
         }
 
     }
+    private void clearFields() {
+        tblCustomer.refresh();
+        txtEmail.clear();
+        txtContact.clear();
+        txtName.clear();
+        txtID.clear();
+        txtID.setEditable(true);
+    }
 
     public void updateBtnOnAction(javafx.event.ActionEvent actionEvent) {
+        CustomerDto dto = new CustomerDto(txtID.getText(),
+                txtName.getText(),
+                txtContact.getText(),
+                txtEmail.getText()
+        );
 
+        try {
+            boolean isUpdated = customerBo.updateCustomer(dto);
+            if (isUpdated){
+                new Alert(Alert.AlertType.INFORMATION,"Customer "+dto.getId()+" Updated!").show();
+                loadCustomerTable();
+                clearFields();
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void saveBtnOnAction(javafx.event.ActionEvent actionEvent) {
+        CustomerDto dto = new CustomerDto(txtID.getText(),
+                txtName.getText(),
+                txtContact.getText(),
+                txtEmail.getText()
+        );
 
+        try {
+            boolean isSaved = customerBo.saveCustomer(dto);
+            if (isSaved){
+                new Alert(Alert.AlertType.INFORMATION,"Customer Saved!").show();
+                loadCustomerTable();
+                clearFields();
+            }
+
+        } catch (SQLIntegrityConstraintViolationException ex){
+            new Alert(Alert.AlertType.ERROR,"Duplicate Entry").show();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void reloadBtnOnAction(javafx.event.ActionEvent actionEvent) {
 
-    }
+
 }
